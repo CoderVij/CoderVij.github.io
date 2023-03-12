2401b9881850df880c10974a5a5d7cc357285426
CoderVij <contactfreakout@gmail.com>
2023-03-13 01:12:33 +0530

    added .txt file for verification mailjet

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const form = document.querySelector('#contact-form');
 
+var script = document.createElement('script');
+script.src = 'https://smtpjs.com/v3/smtp.js';
+script.onload = function() {
+  // Code that uses the functionality of the external script
+};
+document.head.appendChild(script);
+
+
 const submitForm = (event) => {
   event.preventDefault();
 
+  
   const formData = new FormData(event.target);
 
   //formData.append('private_key', "-----BEGIN PRIVATE KEY-----\n AIzaSyDpCkXnB4KX1xSVL1N233xEH9_D6tmfghw \n-----END PRIVATE KEY-----\n");
 
 
-  
+  //https://email-server-side.onrender.com/submit-form   http://localhost:5000/
   axios.post('https://email-server-side.onrender.com/submit-form', formData, {
     withCredentials: true,
     crossDomain: true,
@@ -21,7 +30,21 @@ const submitForm = (event) => {
       console.error(error);
       alert('Error sending message.');
     });
+
     
+/*
+    Email.send({
+      Host : "smtp.gmail.com",
+      Username : "marcusmercer12@gmail.com",
+      Password : "marcus_mercer12",
+      To : 'teamindiefusion@gmail.com',
+      From : "marcusmercer12@gmail.com",
+      Subject : "This is the subject",
+      Body : "And this is the body"
+  }).then(
+    message => alert(message)
+  );
+    */
 };
 
 form.addEventListener('submit', submitForm);
